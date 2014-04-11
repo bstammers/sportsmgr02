ca2449df630ad9b30b02f63792525ef9b318867e
Brad Stammers <brad.stammers@iinet.net.au>
2014-04-11 16:20:10 +1000

    "Set up Foundation, Devise & RSpec"

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,17 @@ module Sportsmgr02
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.controller_specs true
+      g.request_specs true
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
